7ae32dd34435ae21c870ed017fed9338695d989a
Marlus Saraiva <marlus.saraiva@gmail.com>
2019-01-07 10:11:12 -0300

    Set up travis-ci

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Broadway.MixProject do
     [
       app: :broadway,
       version: "0.1.0",
-      elixir: "~> 1.7",
+      elixir: "~> 1.5",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
