9d5d5b55713e9010d2d1048f0cbac38f1fcfaed8
Wojtek Mach <wojtek@wojtekmach.pl>
2019-07-16 11:56:23 +0200

    Add test

---
+++ test/broadway/dummy_producer_test.exs
@@ -0,0 +1,29 @@
+defmodule Broadway.DummyProducerTest do
+  use ExUnit.Case, async: true
+
+  defmodule Handler do
+    def handle_message(_processor, message, _context) do
+      message
+    end
+  end
+
+  test "send message through", c do
+    {:ok, _} =
+      Broadway.start_link(Handler,
+        name: c.test,
+        producers: [
+          default: [
+            module: {Broadway.DummyProducer, []}
+          ]
+        ],
+        processors: [
+          default: [
+            stages: 1
+          ]
+        ]
+      )
+
+    ref = Broadway.test_messages(c.test, [1, 2])
+    assert_receive {:ack, ^ref, [%{status: :ok}, %{status: :ok}], []}
+  end
+end
