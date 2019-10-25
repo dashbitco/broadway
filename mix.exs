c5e8a40b7c3b15fce47035ab75b65cfc1f537a69
José Valim <jose.valim@plataformatec.com.br>
2019-10-25 16:03:18 +0200

    Update CI

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Broadway.MixProject do
   use Mix.Project
 
-  @version "0.4.0"
+  @version "0.5.0"
   @description "Build concurrent and multi-stage data ingestion and data processing pipelines"
 
   def project do
