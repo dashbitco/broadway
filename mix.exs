ca53e7c711527a8f926472bba5dd9bfee2d11fa5
Michael Crumm <mike@crumm.net>
2019-10-25 11:00:37 -0700

    Add doc groups for modules (#128)

--- mix.exs
+++ mix.exs
@@ -47,6 +47,23 @@ defmodule Broadway.MixProject do
       groups_for_extras: [
         Examples: Path.wildcard("guides/examples/*.md"),
         Internals: Path.wildcard("guides/internals/*.md")
+      ],
+      groups_for_modules: [
+        # Ungrouped Modules:
+        #
+        # Broadway
+        # Broadway.Message
+        # Broadway.BatchInfo
+
+        Acknowledgement: [
+          Broadway.Acknowledger,
+          Broadway.CallerAcknowledger,
+          Broadway.NoopAcknowledger
+        ],
+        Producers: [
+          Broadway.DummyProducer,
+          Broadway.TermStorage
+        ]
       ]
     ]
   end
