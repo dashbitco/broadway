1c9db1033fb914521c13523d7fb0b02b223d199f
Andrea Leopardi <an.leopardi@gmail.com>
2019-11-19 17:53:55 +0530

    Don't use select_replace

--- lib/broadway/rate_limiter.ex
+++ lib/broadway/rate_limiter.ex
@@ -44,17 +44,11 @@ defmodule Broadway.RateLimiter do
 
   @impl true
   def handle_info({:reset_limit, allowed}, {broadway_name, interval}) do
-    # Taken from this match spec:
-    # :ets.fun2ms(fn {@row_name, counter} when counter < allowed -> {@row_name, allowed} end)
-    match_spec = [
-      {{@row_name, :"$1"}, [{:<, :"$1", {:const, allowed}}], [{{@row_name, {:const, allowed}}}]}
-    ]
+    was_rate_limited? = get_currently_allowed(broadway_name) <= 0
 
-    # This returns the number of updated rows. If it's 1, it means that we updated the counter
-    # which in turn means that we didn't have any events allowed anymore so producers might
-    # have buffered messages. In that case, we notify the producers that new rate limiting
-    # is available.
-    if :ets.select_replace(table_name(broadway_name), match_spec) == 1 do
+    true = :ets.insert(table_name(broadway_name), {@row_name, allowed})
+
+    if was_rate_limited? do
       producers = Broadway.producer_names(broadway_name)
       Enum.each(producers, &send(&1, {__MODULE__, :reset_rate_limiting}))
     end
