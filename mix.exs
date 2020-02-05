40842947632d93d346c17b8ca4300f216b254983
José Valim <jose.valim@dashbit.co>
2020-02-05 13:25:39 +0100

    Release v0.6.0-rc.0

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Broadway.MixProject do
   use Mix.Project
 
-  @version "0.6.0-dev"
+  @version "0.6.0-rc.0"
   @description "Build concurrent and multi-stage data ingestion and data processing pipelines"
 
   def project do
