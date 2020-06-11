5e20537a4c1035d8d559f6ba51a259079517ff01
Samuel Mullen <samuel@samuelmullen.com>
2020-06-11 15:27:45 -0500

    Test to ensure telemetry_prefix is set for Batcher

--- test/broadway/batcher_test.exs
+++ test/broadway/batcher_test.exs
@@ -22,4 +22,27 @@ defmodule Broadway.BatcherTest do
     %{state: state} = :sys.get_state(pid)
     assert state.subscription_options[:max_demand] == 123
   end
+
+  test "telemtry_prefix is prepends to the event name" do
+    {:ok, pid} =
+      Broadway.Batcher.start_link(
+        [
+          module: __MODULE__,
+          context: %{},
+          type: :producer_consumer,
+          terminator: __MODULE__,
+          resubscribe: :never,
+          batcher: :default,
+          processors: [:some_processor],
+          batch_size: 123,
+          batch_timeout: 1000,
+          partition: 0,
+          telemetry_prefix: [:my_app, :example]
+        ],
+        []
+      )
+
+    %{state: state} = :sys.get_state(pid)
+    assert state.telemetry_prefix == [:my_app, :example, :broadway, :batcher]
+  end
 end
