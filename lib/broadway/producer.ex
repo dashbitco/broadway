f0d6940f284d99792a563a2c2dc3c5ca9fc66622
José Valim <jose.valim@dashbit.co>
2020-08-15 17:01:38 +0200

    Make Broadway.Producer docs public, closes #189

--- lib/broadway/producer.ex
+++ lib/broadway/producer.ex
@@ -1,5 +1,14 @@
 defmodule Broadway.Producer do
-  # TODO: Write docs
+  @moduledoc """
+  A Broadway.Producer is a `GenStage` producer that emits
+  `Broadway.Message` as events.
+
+  Optionally, a `Broadway.Producer` can implement two
+  optional Broadway callbacks: `c:prepare_for_start/2`
+  and `c:prepare_for_draining/2`, which are useful for
+  booting up and shutting down Broadway topologies
+  respectively.
+  """
 
   @doc """
   Invoked once by Broadway during `Broadway.start_link/2`.
