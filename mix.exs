0eb9389dbc8a409a244fe57b4447559443887ed3
Andrea Leopardi <an.leopardi@gmail.com>
2023-03-08 15:24:07 -0600

    Add support for nimble_options ~> 1.0 (#321)

--- mix.exs
+++ mix.exs
@@ -27,7 +27,7 @@ defmodule Broadway.MixProject do
   defp deps do
     [
       {:gen_stage, "~> 1.0"},
-      {:nimble_options, "~> 0.3.7 or ~> 0.4.0 or ~> 0.5.0"},
+      {:nimble_options, "~> 0.3.7 or ~> 0.4 or ~> 1.0"},
       {:telemetry, "~> 0.4.3 or ~> 1.0"},
       {:ex_doc, ">= 0.19.0", only: :docs},
       {:excoveralls, "~> 0.14.4", only: :test}
