0b74efc43c332753c3c135929fbe7348e0161ff9
Brad Hanks <hanks.brad@gmail.com>
2025-04-01 20:39:10 -0600

    * parameter format edits * removing some passive voice and weak words like "should" * minor rewrite for different word choice/sentence structure. *white space changes
    Note: If we don't like the formatting of the `c:prepare_for_start/2` parameters, here are the edits in paragraph form:
    
     The `module` parameter represents the Broadway module passed as the first argument to
      `Broadway.start_link/2`. The `options` parameter contains all the Broadway topology options passed
      as the second argument to `Broadway.start_link/2`.

--- lib/broadway/producer.ex
+++ lib/broadway/producer.ex
@@ -18,43 +18,41 @@ defmodule Broadway.Producer do
   ## Injected Broadway configuration
 
   If `options` is a keyword list, Broadway injects a `:broadway` option
-    into that list. This option contains the configuration for the
-    complete Broadway topology (see `Broadway.start_link/2`). For example,
-    use `options[:broadway][:name]` to uniquely identify the topology.
+  into that list. This option contains the configuration for the
+  complete Broadway topology (see `Broadway.start_link/2`). For example,
+  use `options[:broadway][:name]` to uniquely identify the topology.
 
   The `:broadway` configuration also has an `:index` key. This
   is the index of the producer in its supervision tree (starting
-  from `0`). This allows features such as having even producers
-  connect to some server while odd producers connect to another.
+  from `0`). This enables patterns such as connecting even-indexed producers
+  to one server while odd-indexed producers connect to another.
 
-  If `options` is any other term, it is passed as is to the `c:GenStage.init/1`
-  callback. All other functions behave precisely as in `GenStage`
-  with the requirements that all emitted events must be `Broadway.Message`
-  structs.
-
+  If `options` is any other term besides a keyword list, it is passed directly to the `c:GenStage.init/1`
+  callback without modification. All other functions behave precisely as in `GenStage`
+  with the requirement that all emitted events must be `Broadway.Message` structs.
   ## Optional callbacks
 
   A `Broadway.Producer` can implement two optional Broadway callbacks,
-  `c:prepare_for_start/2` and `c:prepare_for_draining/1`, which are useful
-  for booting up and shutting down Broadway topologies respectively.
+  `c:prepare_for_start/2` and `c:prepare_for_draining/1`, which
+  boot up and shut down Broadway topologies, respectively.
 
   ## Producing Broadway messages
 
   The pipeline modifies `Broadway.Message` structs using functions
   from the `Broadway.Message` module, except for [custom producers](https://hexdocs.pm/broadway/custom-producers.html).
 
-  These fields are:
+  Manipulate these custom producer struct fields directly:
 
     * `:data` (required) - the message data.
       Pass in the `:data` field directly. (Don't use `Broadway.Message.put_data/2`.)
 
-    * `:acknowledger` (required) - the acknowledger of the message, of type
-      `t:Broadway.Message.acknowledger/0`.
+    * `:acknowledger` (required) - the message acknowledger.
+      The acknowledger's type is `t:Broadway.Message.acknowledger/0`.
 
     * `:metadata` (optional) - the producer-attached message metadata.
       Optionally document information for users to use in their pipelines.
 
-  For example, a producer could create a message by doing something like this:
+  For example, a custom producer creates this message:
 
       %Broadway.Message{
         data: "some data here",
@@ -66,25 +64,27 @@ defmodule Broadway.Producer do
   @doc """
   Broadway invokes this callback once during `Broadway.start_link/2`.
 
-  The goal of this callback is to manipulate the general topology options,
-  if necessary at all, and introduce any new child specs that will be
-  started **before** the producers' supervisor in Broadway's supervision tree.
-  Broadway's supervision tree is a `rest_for_one` supervisor (see the documentation
-  for `Supervisor`), which means that if the children returned from this callback
-  crash they will bring down the rest of the pipeline before being restarted.
+  This callback manipulates the general topology options
+  and introduces any new child specifications that Broadway will
+  start **before** the producers' supervisor in its supervision tree.
+  Broadway's supervision tree operates as a `rest_for_one` supervisor
+  (see `Supervisor`), so if the children returned from this callback
+  crash, the entire pipeline shuts down until the supervisor restarts them.
 
-  Broadway invokes this callback within the main Broadway process.
+  Broadway invokes this callback within the main Broadway process with the
+  following parameters:
 
-  `module` is the Broadway module passed as the first argument to
-  `Broadway.start_link/2`. `options` is all of Broadway topology options passed
-  as the second argument to `Broadway.start_link/2`.
+    * `module` - the Broadway module passed as the first argument to `Broadway.start_link/2`.
 
-  The return value of this callback is a tuple `{child_specs, options}`. `child_specs`
-  is the list of child specs to be started under Broadway's supervision tree.
-  `updated_options` is a potentially-updated list of Broadway options
-  that will be used instead of the ones passed to `Broadway.start_link/2`. This can be
-  used to modify the characteristics of the Broadway topology to accommodate
-  the children started here.
+    * `options` - a keyword list of Broadway topology options passed
+      as the second argument to `Broadway.start_link/2`.
+
+  The callback returns a tuple `{child_specs, updated_options}`. `child_specs`
+  is a list of child specifications from the children that Broadway will start under its supervision tree.
+  `updated_options` contains potentially-modified Broadway options
+  that replace those initially passed to `Broadway.start_link/2`. This
+  allows adjustments to the characteristics of the Broadway topology to accommodate
+  the newly introduced children.
 
   ## Examples
 
@@ -119,7 +119,7 @@ defmodule Broadway.Producer do
   must ask the data provider to stop sending messages. Broadway will invoke this for
   each producer stage.
 
-  `state` is the current state of the producer.
+    * `state` - the current state of the producer.
   """
   @callback prepare_for_draining(state :: any) ::
               {:noreply, [event], new_state}
